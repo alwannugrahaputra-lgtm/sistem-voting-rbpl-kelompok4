1cb91808040712f2332791be2bd18ecbb8870a7b
Alwan Nugraha putra <alwannugrahaputra@gmail.com>
2026-03-03 00:26:06 +0700

    Resolve merge conflict dan memisahkan main.py dan voting.py

--- src/voting.py
+++ src/voting.py
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-from voting import Voting
-
-def main():
-    voting = Voting()
-
-    while True:
-        print("\n=== SISTEM VOTING ===")
-        print("1. Tambah Kandidat")
-        print("2. Voting")
-        print("3. Tampilkan Hasil")
-        print("4. Keluar")
-
-        pilihan = input("Pilih menu: ")
-
-        if pilihan == "1":
-            nama = input("Nama kandidat: ")
-            voting.tambah_kandidat(nama)
-
-        elif pilihan == "2":
-            voting.tampilkan_hasil()
-            nomor = int(input("Pilih nomor kandidat: "))
-            voting.vote(nomor)
-
-        elif pilihan == "3":
-            voting.tampilkan_hasil()
-
-        elif pilihan == "4":
-            break
-
-if __name__ == "__main__":
-    main()
-=======
 import json
 from kandidat import Kandidat
 
@@ -81,4 +48,3 @@ class Voting:
                     self.daftar_kandidat.append(kandidat)
         except FileNotFoundError:
             pass
->>>>>>> fitur-statistik
